fd3fe0417ea2717402448bc5a56d225ad5eb9f02
arshia <arshiaask2442@gmail.com>
2025-10-08 13:02:29 +0330

    insert eda_feature file

--- src/data_fetch.py
+++ src/data_fetch.py
@@ -5,8 +5,29 @@ from datetime import datetime
 
 
 def fetch_yahoo(symbol, interval='15m', period='60d'):
+    
+    # download data
     df = yf.download(symbol, interval=interval, period=period)
     df.reset_index(inplace=True)
+    
+    # cleaning dataframe's rows
+    df = df.iloc[2:].copy()
+    
+    # drop Volume columns
+    if 'Volume' in df.columns:
+        df.drop(columns=['Volume'], inplace=True)
+        
+    # creating new columns name    
+    new_columns = ['Datetime', 'Close', 'High', 'Low', 'Open']
+    df.columns = new_columns
+    
+    # removing empty rows
+    df.dropna(inplace=True)
+    
+    # sorting base on time
+    df['Datetime'] = pd.to_datetime(df['Datetime'])
+    df.sort_values('Datetime', inplace=True)
+    
     return df
 
 
@@ -20,6 +41,5 @@ def save_to_db(symbol, df, db_name='forex_data.db'):
 if __name__ == "__main__":
     eur_usd = fetch_yahoo("EURUSD=X", interval="15m", period="60d")
     save_to_db("EURUSD=X", eur_usd)
-    
     print("Data fetch & save in DB form YahooFinance")
 
